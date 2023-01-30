e56ed509814a5c82d959067994549cfd76e8da19
The Major <spartan364@gmail.com>
2023-01-30 11:37:50 -0800

    fix extra comma in navtabs

--- src/components/navtabs.tsx
+++ src/components/navtabs.tsx
@@ -146,7 +146,7 @@ export class NavTabs extends MitrhilTsxComponent<navTabProps> {
             );
         });
 
-        const [[fldrClass, fldrCallback], [packClass, packCallback], [libClass, libCallback], , [glossaryClass, glossaryCallback]] = activeTab.match(baseNavTabMatcher);
+        const [[fldrClass, fldrCallback], [packClass, packCallback], [libClass, libCallback], [glossaryClass, glossaryCallback]] = activeTab.match(baseNavTabMatcher);
         const fldr = "Folder".slice(0, maxNameLen);
         const pack = "Pack".slice(0, maxNameLen);
         const lib = "Library".slice(0, maxNameLen);
